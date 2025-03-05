9784623843fa4cea48f85dd083cd6a51ab5e5ed5
Rahul-k-AD <rahulk.ad24@bitsathy.ac.in>
2025-03-05 10:13:12 +0530

    this is a class component

---
+++ learning/src/components/class.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class android extends React.Component{
+    render( )
+    {
+        this.render()
+        {
+            <h1>
+                Hey this is a class example
+            </h1>
+        }
+    }
+}
+
+
+export default android;
